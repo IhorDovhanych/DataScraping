5d67a331340675c2f076282e2637287877dad7e2
Ihor <93588430+IhorDovhanych@users.noreply.github.com>
2023-04-10 13:32:38 +0300

    Lab4

---
+++ my-hotline/app/Http/Controllers/HotlineItemsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Models\HotlineItem;
+
+
+class HotlineItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = HotlineItem::all();
+        //dd($items[0]['name']);
+        return view('hotline.index', ['items' => $items]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('hotline.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $candidate = HotlineItem::create([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+            'url' => $request->input('url'),
+            'image_urls' => $request->input('image_urls'),
+            'img' => $request->input('img'),
+        ]);
+        return redirect(route('hotline.index'));
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Candidate $candidate)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CandidateRequest $request, Candidate $candidate)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+    }
+}
